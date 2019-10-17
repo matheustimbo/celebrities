d10947a1d95e92d3fb2b1399cfc8f9e45943c472
Lucas A <lucwars@hotmail.com>
2019-10-17 18:38:28 -0300

    telas sem a title bar e celebridades na main

--- android/app/src/main/java/com/example/celebrities/DetailsActivity.kt
+++ android/app/src/main/java/com/example/celebrities/DetailsActivity.kt
@@ -2,11 +2,13 @@ package com.example.celebrities
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.WindowManager
 
 class DetailsActivity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 
 
--- android/app/src/main/java/com/example/celebrities/MainActivity.kt
+++ android/app/src/main/java/com/example/celebrities/MainActivity.kt
@@ -2,7 +2,12 @@ package com.example.celebrities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 
@@ -13,23 +18,40 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         celebritiesAdapter = CelebritiesAdapter(
             onItemClicked = {
                 startActivity<DetailsActivity>()
+               
             }
         )
-        celebritiesList.layoutManager = LinearLayoutManager(this)
+        celebritiesList.layoutManager = LinearLayoutManager(this) as RecyclerView.LayoutManager?
         celebritiesList.adapter = celebritiesAdapter
 
         var celebrities = mutableListOf<Celebrity>(
-            Celebrity("Matela", R.drawable.matela, "Fala dev"),
-            Celebrity("Matela", R.drawable.matela, "Fala dev"),
-            Celebrity("Matela", R.drawable.matela, "Fala dev"),
-            Celebrity("Matela", R.drawable.matela, "Fala dev")
+            Celebrity("Daniel Radcliffe", R.drawable.danielradcliffe, "Fala dev"),
+            Celebrity("Brie Larson", R.drawable.brielarson, "Fala dev"),
+            Celebrity("Rami Malek", R.drawable.ramimalek, "Fala dev"),
+            Celebrity("Tom Holland", R.drawable.tomholland, "Fala dev"),
+            Celebrity("Scarlet jonhason", R.drawable.scarletjonhason, "Fala dev"),
+            Celebrity("Michael Fassbender", R.drawable.michaelfassbender, "Fala dev"),
+            Celebrity("Miranda Cosgrove", R.drawable.mirandacosgrove, "Fala dev"),
+            Celebrity("Joaquin Phoenix", R.drawable.joaquinphoenixjoker, "Fala dev"),
+            Celebrity("Benedict Cumberbatch", R.drawable.benedictcumberbatch, "Fala dev"),
+            Celebrity("James Mcvoy", R.drawable.jamesmcvoy, "Fala dev")
         )
+
+
 
         celebritiesAdapter.refresh(
             celebrities
         )
     }
 }
+// window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+//
+//
+//
+//
+//
